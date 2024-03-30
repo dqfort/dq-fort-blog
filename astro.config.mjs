054d8dbdd2a956ae10c6e591faccc474ce417f90
Fai <qpc167@gmail.com>
2024-03-30 18:47:46 +0800

    Support latex syntax in mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,22 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [
+			[
+				rehypeKatex,
+				{
+					// Katex plugin options
+				}
+			]
+		]
+	},
   integrations: [
     mdx(),
     tailwind({
