b0017735dcc94ba7e4f0ac2ed12a31afaf606d70
Fai <qpc167@gmail.com>
2024-03-30 18:07:00 +0800

    Support react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx(),
     tailwind({
-      applyBaseStyles: false,
-    })
+      applyBaseStyles: false
+    }), 
+    react()
   ]
 });
